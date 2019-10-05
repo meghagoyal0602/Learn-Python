8b2c8e05d1eae5a231d0a102dcad44a9ab87ff2b
MeghaGoyal <meghagoyal0602@gmail.com>
2019-10-05 11:26:54 +0530

    test commit

---
+++ class-1.py
@@ -0,0 +1,21 @@
+# name=input('input name: ')
+# print()
+# print('hello')
+
+# print(name)
+# first_name='Megha'
+# last_name='Goyal'
+# print(first_name last_name)
+# print(first_name +' ' + last_name)
+sentence='My name is Megha Goyal aaaa'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.count('a'))
+first_name=input('what is your name? ')
+last_name=input('what is last name? ')
+# print('hello ' + first_name.capitalize() +' ' + last_name.capitalize())
+# output='hello, '+first_name+last_name
+output=f'hello, {first_name} {last_name}' 
+# output='hello, {} {}'.format(first_name,last_name)
+# output='hello, {0} {1}'.format(first_name,last_name)      
+print(output)
